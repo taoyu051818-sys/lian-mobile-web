eea22730c6699aa779451add0ce56b1eef635a5b
taoyu051818-sys <taoyu051818@gmail.com>
2026-05-04 20:55:16 +0800

    Add LocationChip primitive

---
+++ src/ui/LocationChip.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  icon?: string;
+}>(), {
+  icon: "⌖"
+});
+</script>
+
+<template>
+  <span class="location-chip">
+    <span aria-hidden="true">{{ icon }}</span>
+    <slot />
+  </span>
+</template>
