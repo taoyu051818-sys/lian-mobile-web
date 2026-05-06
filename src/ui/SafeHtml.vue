6cc77959a52dfdb21486fad72a3dc5073550be50
taoyu051818-sys <taoyu051818@gmail.com>
2026-05-06 23:23:52 +0800

    Add SafeHtml wrapper component

---
+++ src/ui/SafeHtml.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { sanitizeHtml } from "../utils/html";
+
+const props = withDefaults(defineProps<{
+  html?: string;
+  as?: "div" | "span" | "p" | "section" | "article";
+}>(), {
+  html: "",
+  as: "div",
+});
+
+const sanitizedHtml = computed(() => sanitizeHtml(props.html));
+</script>
+
+<template>
+  <component :is="as" v-if="sanitizedHtml" v-html="sanitizedHtml" />
+</template>
