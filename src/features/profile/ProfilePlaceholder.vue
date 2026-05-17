448d9f6a089c3a1fed30621bdd9d1dafff7c737b
taoyu051818-sys <taoyu051818@gmail.com>
2026-05-17 15:56:26 +0800

    fix(profile): satisfy profile summary placeholder props

--- src/features/profile/ProfilePlaceholder.vue
+++ src/features/profile/ProfilePlaceholder.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { DEFAULT_PROFILE_SETTINGS, DEFAULT_PROFILE_STATS } from "../../api/profile";
 import ProfileSummary from "./ProfileSummary.vue";
 </script>
 
 <template>
-  <ProfileSummary />
+  <ProfileSummary
+    :stats="DEFAULT_PROFILE_STATS"
+    :settings="DEFAULT_PROFILE_SETTINGS"
+    :loading="false"
+    error=""
+    :has-forum-link="false"
+  />
 </template>
